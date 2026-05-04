fcd081a9b83818b2e8e713b844061fdb5bc412a2
Rupesh-MJ <rupeshtheja84@gmail.com>
2026-05-04 11:42:22 +0530

    Update playwright.config.js

--- playwright.config.js
+++ playwright.config.js
@@ -31,9 +31,9 @@ export default defineConfig({
     /* Base URL to use in actions like `await page.goto('')`. */
     // baseURL: 'http://localhost:3000',
 
-    headless : false, 
-    screenshot:'on',
-    video:'on',
+    // headless : false, 
+    // screenshot:'on',
+    // video:'on',
     viewport:{width:720,height:480},
     //baseURL: 'qa or stg url',
 
